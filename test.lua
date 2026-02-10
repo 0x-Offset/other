bcd857815036d4dd0033da136cdc2e033aa45100
hookmetamethod_hook <st3v3bl0ks@gmail.com>
2026-02-10 17:29:31 +0000

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,2 +1,4 @@
 print("loaded successfully!")
+if printconsole then printconsole("loaded successfully") end -- RoExec
+if rconsoleprint then rconsoleprint("loaded successfully") end -- exploits
 return true
